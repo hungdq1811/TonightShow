cac36205f4f62a3ca7af3916e880e6cb61ceef61
BurNova <burnova@gmail.com>
2014-06-01 15:59:45 +0700

    update mongodb

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
+var mongo = requires('mongodb');
+
+MongoClient = require('mongodb').MongoClient;
+Server = require('mongodb').Server;
+
+var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/mydb';
+
+mongo.Db.connect(mongoUri, function(error, db){
+	db.collection('mydocs', function(err, collection){
+		collection.insert({"key":"value"}, {safe: true}, function(err, rs){
+			//none
+		});
+	});
+});
 
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('Hello World! Blah Blah Blah!');
+  res.send('Hello World! 1');
 });
 
 var port = Number(process.env.PORT || 5000);
